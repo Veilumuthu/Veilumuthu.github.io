f8cf62b1d58da3787c9108cf9eeec45c5bc21c40
Veilu muthu <dambldore28@gmail.com>
2020-08-11 13:30:33 +0530

    init

---
+++ precache-manifest.8fd012071d6879abef7e6ee5819d291b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff729015a8db0744540a01dbf21f91f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4cbb3698dbf9b2e1fc2",
+    "url": "/static/css/main.630be68b.chunk.css"
+  },
+  {
+    "revision": "285983c30d609b462e01",
+    "url": "/static/js/2.bea71168.chunk.js"
+  },
+  {
+    "revision": "fb8e993bf740f49439948eabe43f0e99",
+    "url": "/static/js/2.bea71168.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4cbb3698dbf9b2e1fc2",
+    "url": "/static/js/main.71be61fa.chunk.js"
+  },
+  {
+    "revision": "f438879c68bfc8864397",
+    "url": "/static/js/runtime-main.cc36b19e.js"
+  }
+]);
